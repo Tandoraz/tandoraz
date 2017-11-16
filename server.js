b5f2293876071af0a80d2a43b0e51fabac9a8bbb
Tandoraz <buergi.tobias@gmail.com>
2017-11-16 22:46:43 +0100

    update server and some initial website look

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 var port = 3000;
-app.use(express.static(__dirname + '/app'));
+app.use(express.static(__dirname));
+app.get('/', function(req, res) {
+    res.redirect('/app');
+});
 app.listen(port);
 console.log('server started on: localhost:' + port);
