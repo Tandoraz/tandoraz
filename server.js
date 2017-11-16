7c4bb298cac92018239d285eef1cc4a5e80d4d58
Tandoraz <buergi.tobias@gmail.com>
2017-11-16 21:51:29 +0100

    adding expressjs for nodejs server

---
+++ server.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+app.use(express.static(__dirname + '/app'));
+app.listen(port);
+console.log('server started on: localhost:' + port);
