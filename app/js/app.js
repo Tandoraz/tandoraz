7126a53f6fbb0dda64bbf829dcd4b76b0884d117
Tandoraz <buergi.tobias@gmail.com>
2017-11-19 02:25:55 +0100

    changed favicon and finished styling the website

--- app/js/app.js
+++ app/js/app.js
@@ -2,6 +2,18 @@ function navigateTo(section) {
   $('html, body').animate({scrollTop: $('#' + section).offset().top - 60 }, 'slow');
 }
 
+var birthday = {
+  day: 08,
+  month: 04,
+  year: 1998
+}
+var now = new Date();
+var age = now.getFullYear() - birthday.year;
+if (now.getMonth() < birthday.month || (now.getMonth() == birthday.month && now.getDate() < birthday.day)) {
+  age--;
+}
+document.getElementById("age").innerHTML = age;
+
 $(document).ready(function() {
   $('body').scrollspy({ target: '#nav' , offset: 60});
 });
