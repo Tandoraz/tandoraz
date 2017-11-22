3d89d07d0d9cfdf08c95f8d63fba93ebebeebbf4
Tandoraz <buergi.tobias@gmail.com>
2017-11-22 23:02:25 +0100

    added webpack

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
 var port = 3000;
-app.use(express.static(__dirname + '/node_modules'));
-app.use(express.static(__dirname + '/app'));
+app.use(express.static(__dirname + '/dist'));
 app.listen(port);
 console.log('server started on: localhost:' + port);
